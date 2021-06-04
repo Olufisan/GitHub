0c5b06cecf23178fb3b083d8127adce1e88b43a7
Olufisan <fisan05@yahoo.co.uk>
2021-06-04 16:35:28 +0100

    second commit

---
+++ src/test/java/Last/Straw/Demo.java
@@ -0,0 +1,16 @@
+package Last.Straw;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		System.out.println("I'm good with this application");
+		System.out.println("Thank you selenium");
+		System.out.println("Mision Imposible 1");
+		System.out.println("Mision Imposible 2");
+		System.out.println("Mision Imposible 3");
+		System.out.println("Mision Imposible 4");
+		System.out.println("Mision Imposible 5");
+
+	}
+
+}
